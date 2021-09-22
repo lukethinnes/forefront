24eb237bb6ccc2155d15d8762eb2fdbac36a7d42
Luke Thinnes <lukethinnes@yahoo.com>
2021-09-22 17:23:20 -0600

    Added delete functionality passed by props.

---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+export default class Counter extends Component {
+  state = {
+    value: this.props.value,
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: 'bold',
+  };
+
+  handleIncrement = () => {
+    this.setState({value: this.state.value + 1});
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()} style={this.styles}>
+          {this.formatCount()}
+        </span>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={this.handleIncrement}>
+          Increment
+        </button>
+        <button class="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.id)}>
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 bg-';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
+    return classes;
+  }
+
+  formatCount() {
+    const {value} = this.state;
+    return value === 0 ? 'Zero' : value;
+  }
+}
---
+++ src/components/counters.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Counter from './counter';
+
+export default class Counters extends Component {
+  state = {
+    counters: [
+      {id: 1, value: 0},
+      {id: 2, value: 0},
+      {id: 3, value: 0},
+      {id: 4, value: 0},
+    ],
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            id={counter.id}
+            key={counter.id}
+            onDelete={this.handleDelete}
+            value={counter.value}
+          />
+        ))}
+      </div>
+    );
+  }
+}
