9f6681d7912e1c3b3a8af44f3438842d997e10c8
Luke Thinnes <lukethinnes@yahoo.com>
2021-09-22 09:01:28 -0600

    Rendered API information in bootstrap table.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import React, {Component} from 'react';
+import Movies from './components/movies';
 
 function App() {
   return (
-  <main className='container'>
-    <h1>Hello World</h1>
-  </main>
+    <main className="container">
+      <Movies />
+    </main>
   );
 }
 
