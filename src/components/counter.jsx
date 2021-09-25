d1dc91302b0d174f3fc221a51d9c4204161031b6
Luke Thinnes <lukethinnes@yahoo.com>
2021-09-24 18:44:52 -0600

    Added reset button & reset handler.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -25,7 +25,7 @@ export default class Counter extends Component {
           onClick={this.handleIncrement}>
           Increment
         </button>
-        <button class="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>
+        <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>
           Delete
         </button>
       </div>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -16,9 +16,18 @@ export default class Counters extends Component {
     this.setState({counters});
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({counters});
+  };
+
   render() {
     return (
       <div>
+        <button className="btn btn-primary btn-sm m-2">Reset</button>
         {this.state.counters.map(counter => (
           <Counter
             counter={counter}
