06d95144b3fe2f9352b0d15885354b88196630ec
Luke Thinnes <lukethinnes@yahoo.com>
2021-10-04 09:28:30 -0600

    Applied the handleReset to reset button.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -35,7 +35,7 @@ export default class Counters extends Component {
   render() {
     return (
       <div>
-        <button className="btn btn-primary btn-sm m-2">Reset</button>
+        <button className="btn btn-primary btn-sm m-2" onClick={this.handleReset}>Reset</button>
         {this.state.counters.map(counter => (
           <Counter
             counter={counter}
